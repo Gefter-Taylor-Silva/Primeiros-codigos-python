235f878d82dbc2862e0a29ff8c4a3e747ff7136a
Gefter-Taylor-Silva <geftersilva10@gmail.com>
2025-06-15 07:21:22 -0300

    Create Calculadora_soma.py

---
+++ Calculadora_soma.py
@@ -0,0 +1,8 @@
+print("Calculadora de Soma")
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+
+print(f"A soma é: {soma}")
